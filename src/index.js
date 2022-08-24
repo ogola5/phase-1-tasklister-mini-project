e6b78c35caaa2441ec6a7689e87bac79fdbc59a4
ogola5 <ogolaevance5@gmail.com>
2022-08-24 21:50:33 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  document.querySelector('form').addEventListener('submit',(e)=>{
+    e.preventDefault()
+    buildtodo(e.target.input.value)
+  })
+  
+})
+
+function buildtodo(todo){
+
+  let p = document.createElement('p')
+  p.textContent = todo
+  document.querySelector('#list').appendChild(p)
+}
